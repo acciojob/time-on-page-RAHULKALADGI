f5af2d62ef0b2e976b31df85c23a4410356b461e
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-10 21:21:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 
-import React from "react";
+import React, {useState , useEffect} from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App =()=> {
+let [time , setTime] = useState(0);
+
+setInterval(()=> {
+  setTime(()=>time+1);
+},1000)
+
+useEffect(()=> {
+  setTime(0);
+} , []);
+
+return (
+  <div>
+    <p>You've been on this page for {time} seconds.</p>
+  </div>
+)
+
 }
 
 export default App
